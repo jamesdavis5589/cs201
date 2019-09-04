1dc22554f1f169df90ef19c4d1cc231d15c700ec
James <54640322+jamesdavis5589@users.noreply.github.com>
2019-09-04 10:17:08 -0800

    Add files via upload

---
+++ diamond.cpp
@@ -0,0 +1,29 @@
+/**
+ *James Davis
+ *September 4th
+ *CS201
+ *Diamond.cpp
+ *This program will take user input int, then output a diamond shape with a corresponding size
+*/
+
+#include <iostream>
+
+int main
+{
+	int x;
+	cout << "Please input an integer\n";
+	cin >> x;
+
+	//check if input is an integer
+	if (x)  //if x isn't an integer
+	{
+	cout << "That was not an integer, please try again\n";
+	//recursively restart program
+	}
+
+	//print out diamond
+	int i = x;
+	while
+
+
+}
