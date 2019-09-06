d71342098a70d32d58534740b7d57d0c75c46ce1
James Davis <james.davis5589@outlook.com>
2019-09-06 10:00:58 -0800

    upload updated HW1 diamond

--- HW1diamond.cpp
+++ HW1diamond.cpp
@@ -8,22 +8,44 @@
 
 #include <iostream>
 
-int main
+//creating a method for user input
+public int x;
+public static void ask()
 {
-	int x;
 	cout << "Please input an integer\n";
 	cin >> x;
 
 	//check if input is an integer
-	if (x)  //if x isn't an integer
+	if (x != int)
 	{
-	cout << "That was not an integer, please try again\n";
-	//recursively restart program
+		cout << "That was not an integer, please try again\n";
+		ask();   //asks the user for another input
 	}
+}
+
+int main
+{
+	ask();
 
 	//print out diamond
-	int i = x;
-	while
+	int r;
+	for (int i = 0, i < x, i++)
+	{
+		for (int p = 0, p < i, p++)
+		{
+			cout << "#";
+		}
+		cout << "\n";
+		r = i;
+	}
 
+	for (int i = r--, i > 0, i--)
+	{
+		for (int p = 0, p < i, p++)
+		{
+			cout << "#";
+		}
+		cout << "\n";
+	}
 
 }
