96076bce46e02d83e2a2909c1cc3b513965a1487
James Davis <james.davis5589@outlook.com>
2019-09-09 09:44:57 -0800

    lab 3 is about done, updating

--- Lab 3/lab3.cpp
+++ Lab 3/lab3.cpp
@@ -3,13 +3,33 @@ James Davis
 CS201
 September 9th
 Lab3.cpp
-This program will print out numbers 1-20, as well as print out a 10x20 box
+This program will print out numbers 1-20, as well as print out a 60x10 box
 */
 
 import <iostream>
 
 public main
 {
+	//print 1-20
+	int x = 1;
+	while (x <= 20)
+	{
+		cout << x
+		x++;
+	}
 
+	//print out the box
+	int j = 0;
+	int k = 0;
+	while (j<10)
+	{
+		while (k < 60)
+		{
+			if (k % 2 == 0)
+				cout << "X";
 
+			if (k % 2 == 1)
+				cout << "D";
+		}
+	}
 }
