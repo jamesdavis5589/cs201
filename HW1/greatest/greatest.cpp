17e5fc12a88d7754a3e97e9202ce8a04bf51425d
James Davis <james.davis5589@outlook.com>
2019-09-15 17:37:51 -0800

    dun did greatest.cpp

--- HW1/greatest/greatest.cpp
+++ HW1/greatest/greatest.cpp
@@ -6,7 +6,26 @@ HW! - greatest.cpp
 This program will
 */
 
+#include <iostream>
+
 int main()
 {
+	int x;
+	int big=1;
+	int i = 24;
 
+	while(i!=0)
+	{
+		std::cout << "Please enter an integer, or enter 0 to end\n";
+		std::cin >> x;
+
+		if (x > big)
+			big = x;
+
+		if (x <= 0)
+			break;
+	}
+
+	std::cout << "The largest integer you entered was ";
+	std::cout << big;
 }
