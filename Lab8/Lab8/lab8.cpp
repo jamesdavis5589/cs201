37408e1b4f6aa62f3703b1666a1e112b4f15aefe
James Davis <james.davis5589@outlook.com>
2019-09-20 10:06:36 -0800

    created lab8

---
+++ Lab8/Lab8/lab8.cpp
@@ -0,0 +1,30 @@
+/*
+James Davis
+CS201
+September 20th
+lab8.cpp
+This program will define preestablished functions declared in separate .hpp files
+*/
+
+#include lab8.hpp
+#include intio.hpp
+
+int doInput()
+{
+	std::cout << "Please input an integer:";
+	return getInt();
+}
+
+int compute(int x)
+{
+	if (x <= 0)
+		return 0
+
+	int count=-1;
+	for (int i = 1; i < x; i++)
+	{
+		if (x % i == 0)
+			count++;
+	}
+	return count;
+}
