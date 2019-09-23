3895e9b251b3d2a54162e7e112c0035487f563bf
James Davis <james.davis5589@outlook.com>
2019-09-22 18:22:49 -0800

    i has made works still need more

--- HW2/names/names.cpp
+++ HW2/names/names.cpp
@@ -12,7 +12,44 @@ This prgram will
 #include <string>
 #include <vector>
 
-int main()
+void inputnames(std::vector<std::string> & names)
 {
+	std::string x;
+	std::cin >> x;
+	vector.pushBack(x);
+}
 
+bool doesNameExist(const std::string & nametoFind, const std::vector<std::string> & names)
+{
+	for (int i = 0; i < vector.size(); i++) //replace vector.size()
+	{
+		if (string == vector[i]) //replace string & vector[i]
+		{
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+
+void printNames(const std::vector<string> & names)
+{
+	for (int i = 0; i < vector.size(); i++) //replace vector.size()
+	{
+		std::cout << vector[i]; //replace vector[i]
+		std::cout << "\n";
+	}
+}
+
+int main(int argc, char **argv)
+{
+	std::vector < std::string::names;
+	for (int i = 0; i < 10; i++)
+	{
+		std::string name;
+		std::cout << "Please enter a name:\n";
+		std::getline(cin, name);
+		names.push_back(name);
+	}
+	return 0;
 }
