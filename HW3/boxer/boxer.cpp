35e358b08c6f461b2b463d56cf6a9616da3c1022
James Davis <james.davis5589@outlook.com>
2019-09-23 09:48:53 -0800

    established hw3 boxer

---
+++ HW3/boxer/boxer.cpp
@@ -0,0 +1,20 @@
+/*
+James Davis
+CS201
+September 25th
+HW# - boxer.cpp
+This program will create various sized boxes around input text
+*/
+
+#include <iostream>
+#include <string>
+
+void boxer(std::string word, int thick)
+{
+
+}
+
+int main()
+{
+
+}
