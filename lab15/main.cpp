1b524058b3036eeb3857c9a4d4c1d823cf5eb20c
James Davis <james.davis5589@outlook.com>
2019-10-07 10:15:09 -0800

    cleaned up a bit

--- lab15/main.cpp
+++ lab15/main.cpp
@@ -8,17 +8,30 @@ This program will print the numbers in a string using string stream
 
 #include "Header.h"
 
+bool errorCheck(std::vector <std::string> v)
+{
+	bool error;
+	int errorCount = 0;
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (!pnum(v[i]))
+		{
+			std::cout << "ERROR: value located at slot " << i + 1 << " is causing a string stream error.\n";
+			error = true;
+			errorCount++;
+		}
+	}
+
+	std::cout << "Number of errors: " << errorCount << "\n";
+	return error;
+}
+
 int main()
 {
 	std::vector <std::string> ex = { "69", "69a", "a69", "5.6", "    68", "\t11", "68 69", "0xA" };
 
-	for (int i = 0; i < ex.size(); i++)
-	{
-		if (!pnum(ex[i]))
-		{
-			std::cout << "ERROR: value located at slot " << i+1 << " is causing a string stream error.\n";
-		}
-	}
+	errorCheck(ex);
 
 	for (std::string s: ex)
 	{
