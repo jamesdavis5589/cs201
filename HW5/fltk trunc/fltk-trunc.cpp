190db2d68ad9427a3e6bc10817ff847674ff170e
James Davis <james.davis5589@outlook.com>
2019-11-03 10:52:57 -0900

    Got fltk installed! (dont know if itll carry into the github tho)

--- HW5/fltk trunc/fltk-trunc.cpp
+++ HW5/fltk trunc/fltk-trunc.cpp
@@ -1,14 +1,24 @@
 /*
 James Davis
 CS201
-October 29th
-HW3 - fltk-trunc.cpp
+November 3rd
+HW5 - fltk-trunc.cpp
 This program will
 */
 
 #include <iostream>
+#include <FL/Fl_Toggle_Round_Button.H>
+#include <FL/Fl_Round_Button.H>
+#include <FL/Fl_Return_Button.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Widget.H>
 
 int main()
 {
+	//default dimensions of stuff
+	const int dh = 100;
+	const int dw = 300;
 
+	Fl_Window* window = new Fl_Window(dw, dh);
+	window->begin();
 }
