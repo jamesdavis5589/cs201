079d71b419b4a417b201bc4556d60ca600bc2db7
James Davis <james.davis5589@outlook.com>
2019-11-06 10:12:09 -0900

    started file

---
+++ lab27/main.cpp
@@ -0,0 +1,23 @@
+/*
+James Davis
+CS201
+November 6th
+lab 27 - main.cpp
+This program will
+*/
+
+#include <iostream>
+#include <string>
+#include <random>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::mt19937;
+using std::random_device;
+
+int main()
+{
+	random_device ran;
+	mt19937 gen(ran);
+}
