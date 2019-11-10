78be7aa850124dca5bccfecf2f6997753ce1864e
James Davis <james.davis5589@outlook.com>
2019-11-10 12:23:42 -0900

    I did it :3

--- Hw6/random-map/random-map.cpp
+++ Hw6/random-map/random-map.cpp
@@ -44,28 +44,11 @@ int RandomBetween(int first, int last)
 	return rand() % last + first;
 }
 
-void PrintDistribution(const map<int, int> &numbers, int min, int max)
+void PrintDistributionN(int min, int max)
 {
-	
-}
-
-int main()
-{
-	/*
-	// Choose a random mean between 1 and 6
-	std::default_random_engine e1(r());
-	std::uniform_int_distribution<int> uniform_dist(1, 6);
-	*/
-
-	// Seed with a real random value, if available
 	random_device r;
 
-	//My stuff
-	int mean = RandomBetweenN(1, 6);
-	//int mean = RandomBetweenU(1, 6);
-	//int mean = RandomBetween(1, 6);
-
-	//FIRST RANDOM
+	int mean = RandomBetweenN(min, max);
 
 	std::cout << "Randomly-chosen mean: " << mean << '\n';
 
@@ -83,4 +66,69 @@ int main()
 		std::cout << std::fixed << std::setprecision(1) << std::setw(2)
 			<< p.first << ' ' << std::string(p.second / 200, '*') << '\n';
 	}
+}
+
+void PrintDistributionU(int min, int max)
+{
+	random_device r;
+
+	int mean = RandomBetweenU(min, max);
+
+	std::cout << "Randomly-chosen mean: " << mean << '\n';
+
+	// Generate a normal distribution around that mean
+	std::seed_seq seed2{ r(), r(), r(), r(), r(), r(), r(), r() };
+	std::mt19937 e2(seed2);
+	std::normal_distribution<> normal_dist(mean, 2);
+
+	std::map<int, int> hist;
+	for (int n = 0; n < 10000; ++n) {
+		++hist[std::round(normal_dist(e2))];
+	}
+	std::cout << "Normal distribution around " << mean << ":\n";
+	for (auto p : hist) {
+		std::cout << std::fixed << std::setprecision(1) << std::setw(2)
+			<< p.first << ' ' << std::string(p.second / 200, '*') << '\n';
+	}
+}
+
+void PrintDistribution(int min, int max)
+{
+	random_device r;
+
+	int mean = RandomBetween(min, max);
+
+	std::cout << "Randomly-chosen mean: " << mean << '\n';
+
+	// Generate a normal distribution around that mean
+	std::seed_seq seed2{ r(), r(), r(), r(), r(), r(), r(), r() };
+	std::mt19937 e2(seed2);
+	std::normal_distribution<> normal_dist(mean, 2);
+
+	std::map<int, int> hist;
+	for (int n = 0; n < 10000; ++n) {
+		++hist[std::round(normal_dist(e2))];
+	}
+	std::cout << "Normal distribution around " << mean << ":\n";
+	for (auto p : hist) {
+		std::cout << std::fixed << std::setprecision(1) << std::setw(2)
+			<< p.first << ' ' << std::string(p.second / 200, '*') << '\n';
+	}
+}
+
+int main()
+{
+	int min = 2;
+	int max = 9;
+
+	cout << "Uniform Distribution:" << endl;
+	PrintDistributionU(min, max);
+	cout << "\n\n" << endl;
+
+	cout << "Normal Distribution:" << endl;
+	PrintDistributionN(min, max);
+	cout << "\n\n" << endl;
+
+	cout << "Purely Random Distribution:" << endl;
+	PrintDistribution(min, max);
 }
