a137a673035d25f8243d787c761feade5fbb7c1c
James Davis <james.davis5589@outlook.com>
2019-11-10 14:26:58 -0900

    created file

---
+++ Hw6/hangman/hangman.cpp
@@ -0,0 +1,18 @@
+/*
+James Davis
+CS201
+November 10th
+HW6 - hangman.cpp
+This program will play hangman, using lambda functions
+*/
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+int main()
+{
+
+}
