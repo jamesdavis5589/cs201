4655493a3276a1c4924f65af1e3c345a2a12b1a3
James Davis <james.davis5589@outlook.com>
2019-11-25 10:08:27 -0900

    started project

---
+++ lab35/student.cpp
@@ -0,0 +1,9 @@
+/*
+James Davis
+CS201
+November 25th
+lab 35 - student.cpp
+This program will
+*/
+
+#include "student.h"
---
+++ lab35/student.h
@@ -0,0 +1,15 @@
+/*
+James Davis
+CS201
+November 25th
+lab 35 - student.h
+This program will
+*/
+
+#ifndef student_h
+#define student_h
+
+
+
+#endif // !student_h
+
