a73e4c0e137caf72063ec19df51a0772541e24b3
James Davis <james.davis5589@outlook.com>
2019-11-25 21:34:24 -0900

    finished the lab, with functioning class

--- lab35/student.hpp
+++ lab35/student.hpp
@@ -3,15 +3,26 @@ James Davis
 CS201
 November 25th
 lab 35 - student.h
-This program will
+This program will define the class student
 */
 
 #ifndef student_hpp
 #define student_hpp
 
-class students
-{
+#include <string>
 
+using std::string;
+
+class Student
+{
+	string _name;
+	int _id;
+
+public:
+	Student();
+	Student(const string &s);
+	Student(const string &s, const int &n);
+	void printInfo() const;
 };
 
 #endif // !student_hpp
