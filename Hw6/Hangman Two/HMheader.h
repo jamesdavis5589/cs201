3d6d469d60ea6d066b1e04bc6e90dffa51f23a60
James Davis <james.davis5589@outlook.com>
2019-12-01 07:44:00 -0900

    added lose function

--- Hw6/Hangman Two/HMheader.h
+++ Hw6/Hangman Two/HMheader.h
@@ -6,8 +6,33 @@ HW6 - Hangman - HMheader.h
 This program will
 */
 
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+
 #ifndef HMheader_h
 #define HMheader_h
+
+//for when you inevitably lose
+auto lose = []()
+{
+	cout << "Uh uh, you lost  =/";
+	cout << "\n\n";
+	cout << "Want to go again?";
+
+	string Lin;
+	cin >> Lin;
+
+	if (Lin == "y" || Lin == "yes")
+	{
+		main();
+	}
+};
+
+string answer = "stinky"; //Don't tell anyone
 
 #endif // !HMheader_h
 
--- Hw6/Hangman Two/HMmain.cpp
+++ Hw6/Hangman Two/HMmain.cpp
@@ -10,12 +10,15 @@ This program will
 
 int main()
 {
+	string input;
+
 	//Main user input
 	for (int i = 0; i < 10; i++)
 	{
-
+		cout << "What is your next character?" << endl;
+		cin >> input;
 	}
 
 	//lose after 10 turns
-
+	lose;
 }
