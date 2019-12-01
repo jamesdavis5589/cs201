213cd38a356b4cd970992976c95c72d192130ccd
James Davis <james.davis5589@outlook.com>
2019-12-01 07:59:02 -0900

    added charSet

--- Hw6/Hangman Two/HMheader.h
+++ Hw6/Hangman Two/HMheader.h
@@ -12,9 +12,15 @@ using std::cout;
 using std::endl;
 #include <string>
 using std::string;
+#include <vector>
+using std::vector;
 
 #ifndef HMheader_h
 #define HMheader_h
+
+string answer = "stinky"; //Don't tell anyone
+
+vector<char> Canswer;
 
 //for when you inevitably lose
 auto lose = []()
@@ -32,7 +38,34 @@ auto lose = []()
 	}
 };
 
-string answer = "stinky"; //Don't tell anyone
+//Gathers different characters from answer
+auto charSet = [](string)
+{
+	Canswer.push_back(answer[0]);
+	bool boo;
+
+	//Current letter in answer
+	for (int c = 1; answer.size(); c++)
+	{
+		//Checks if letter is already registered
+		for (int d = 0; d < Canswer.size(); d++)
+		{
+			boo = true;
+
+			//Is letter found?
+			if (answer[c] == Canswer[d])
+			{
+				boo = false;
+			}
+
+			//If letter is new
+			if (boo)
+			{
+				Canswer.push_back(answer[c]);
+			}
+		}
+	}
+};
 
 #endif // !HMheader_h
 
--- Hw6/Hangman Two/HMmain.cpp
+++ Hw6/Hangman Two/HMmain.cpp
@@ -10,13 +10,20 @@ This program will
 
 int main()
 {
+	charSet;
+
 	string input;
 
 	//Main user input
 	for (int i = 0; i < 10; i++)
 	{
+		//Input
 		cout << "What is your next character?" << endl;
 		cin >> input;
+		cout << "\n\n";
+
+		//Check input
+
 	}
 
 	//lose after 10 turns
