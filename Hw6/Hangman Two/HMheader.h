3ef4fbaacc483d42959608f000ae726acaf3fbda
James Davis <james.davis5589@outlook.com>
2019-12-01 07:36:02 -0900

    created second rendition of Hangman

---
+++ Hw6/Hangman Two/HMheader.h
@@ -0,0 +1,13 @@
+/*
+James Davis
+December 1st
+CS201
+HW6 - Hangman - HMheader.h
+This program will
+*/
+
+#ifndef HMheader_h
+#define HMheader_h
+
+#endif // !HMheader_h
+
---
+++ Hw6/Hangman Two/HMmain.cpp
@@ -0,0 +1,21 @@
+/*
+James Davis
+December 1st
+CS201
+HW6 - Hangman - HMmain.cpp
+This program will
+*/
+
+#include "HMheader.h"
+
+int main()
+{
+	//Main user input
+	for (int i = 0; i < 10; i++)
+	{
+
+	}
+
+	//lose after 10 turns
+
+}
