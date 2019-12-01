42c08f2a890f23926b65cd1698b8e21d3466538b
James Davis <james.davis5589@outlook.com>
2019-12-01 13:44:38 -0900

    did everything except for inner product

--- Hw6/math-catch/math-catch.cpp
+++ Hw6/math-catch/math-catch.cpp
@@ -8,7 +8,38 @@ This program will do stuff with stuff from Catch2
 
 #include "catch2/catch.hpp"
 
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <numeric>
+using std::accumulate;
+using std::inner_product;
+
 int main()
 {
+	//SINE
+	cout << "sin(pi/4)" << endl;
+	cout << sin(3.1415/4);
+	cout << "\n\n";
+	
+	//ATAN2
+	cout << "atan2(20, -20)" << endl;
+	cout << atan2(20, -20);
+	cout << "\n\n";
 
+	//accumulate()
+	int accArray[] = { 1, 2, 3 };
+
+	cout << "accumulate(accArray, accArray + 3, 30)" << endl;
+	cout << accumulate(accArray, accArray + 3, 30);
+	cout << "\n\n";
+	
+	//inner-product
+	int array1[] = { 5, 10, 15 };
+	int array2[] = { 1, 2, 3 };
+
+	cout << "inner-product(array1[0], array1[2], array2[0], 20)" << endl;
+	cout << inner_product(array1[0], array1[2], array2[0], 20);
 }
