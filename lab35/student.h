609586650d8c8528d773c582bd4265b0e0a33505
James Davis <james.davis5589@outlook.com>
2019-12-02 09:50:42 -0900

    started lab

---
+++ lab35/student.h
@@ -0,0 +1,15 @@
+/*
+James Davis
+CS201
+November 25th
+lab 35 - student.h
+This program will
+*/
+
+#ifndef student_hpp
+#define student_hpp
+
+
+
+#endif // !student_hpp
+
---
+++ lab36/sandwich.cpp
@@ -0,0 +1,17 @@
+/*
+James Davis
+December 2nd
+CS201
+lab 36 - sandwich.cpp
+This program will
+*/
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+}
