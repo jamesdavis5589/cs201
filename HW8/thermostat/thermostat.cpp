381445637534819e330d01b300c1fec5b9b3d8b2
James Davis <james.davis5589@outlook.com>
2019-12-07 22:53:14 -0900

    Defined main, errors occur when running program

--- HW8/thermostat/thermostat.cpp
+++ HW8/thermostat/thermostat.cpp
@@ -10,7 +10,30 @@ This program will run the main thermostat program
 #include "Simulator.h"
 #include "Environment.h"
 
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+
 int main()
 {
+	int dt = 60;
+	int dg = 70;
 
+	cout << "Start of thermostat..." << endl << endl;
+
+	Simulator sim(dt, dg);
+	cout << "Default thermostat set to " << dg << " degrees..." << endl;
+	cout << "Default room tempurature set to " << dt << " degrees..." << endl;
+	cout << endl;
+
+	//Main user loop
+	bool go = true;
+	while (go)
+	{
+		sim.askOwner();
+		sim.run();
+	}
 }
